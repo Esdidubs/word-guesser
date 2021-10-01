3c22cf4fbf76d8aedac621364ed185ad68b73622
Steven Ward <sward3644@gmail.com>
2021-10-01 14:24:42 -0700

    Removed old database things

--- elements.js
+++ elements.js
@@ -1,18 +1,3 @@
-const possibleChars = {
-    lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-
-    upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-
-    num: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-
-    // didn't include &, <, or >
-    sym: ['~', '`', '!', '@', '#', '$', '%', '^', '*', '(', ')', '_',
-            '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '"',
-            "'", ',', '.', '?', '/']
-};
-
 // https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
 const passphrases = [
     'abacus', 'abdomen', 'abdominal', 'abide', 'abiding', 'ability', 'ablaze', 'able',
